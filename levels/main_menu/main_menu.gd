c79774a5a9c1c0ae2b2e8da0e27176cec6d9c61a
Bernard B. <bbausa1203@gmail.com>
2025-10-24 15:33:23 -0700

    Start and Quit Main menu Buttons
    Implemented Play button to start level and Quit button to terminate the game from main menu

---
+++ levels/main_menu/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@export var level_scene: PackedScene
+
+func start_game() -> void:
+	get_tree().change_scene_to_packed(level_scene)
+	
+func end_game() -> void:
+	get_tree().quit()
