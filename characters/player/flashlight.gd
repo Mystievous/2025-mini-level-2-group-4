483789fae5b0c7caf1e2fe14ba23a5cafb79ad10
Mysti <mystievous@starseekstudios.com>
2025-10-29 17:04:28 -0700

    finish player animations and integrate with the player scene (#2)

---
+++ characters/player/flashlight.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	# make the flashlight look at the mouse
+	look_at(get_global_mouse_position())
