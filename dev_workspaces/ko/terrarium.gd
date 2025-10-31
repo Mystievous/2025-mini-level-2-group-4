cdf90b68d33e55852d83c080ba28169ac758737c
ko545 <acadenaperales3860@sdsu.edu>
2025-10-31 08:37:59 -0700

    placing all items in the terrarium triggers win screen

--- dev_workspaces/ko/terrarium.gd
+++ dev_workspaces/ko/terrarium.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
+signal all_items_placed
+
+@export var win_scene: PackedScene
+
 @onready var cap: Sprite2D = $items/cap
 @onready var leaf: Sprite2D = $items/leaf
 @onready var doll: Sprite2D = $items/doll
 @onready var wheel: Sprite2D = $items/wheel
 @onready var skull: Sprite2D = $items/skull
+
+var items_placed : int = 0 
+
+func _process(_delta: float) -> void:
+	if items_placed == 5:
+		print("all items placed")
+		all_items_placed.emit()
+		get_tree().change_scene_to_packed(win_scene)
+		items_placed += 1
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("item_drop"):
@@ -19,4 +32,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 				leaf.visible = true
 			"Doll":
 				doll.visible = true
+		items_placed += 1
 		area.queue_free()
